bf7acc1cfb40a68d76099a7e53d2fee69f813ad8
Gordon Murray <gordon@gordonmurray.com>
2022-04-02 20:25:02 +0100

    dns records for kafka records

--- route53.tf
+++ route53.tf
@@ -2,10 +2,10 @@ resource "aws_route53_zone" "domain" {
   name    = var.domain_name
   comment = "staging"
 
-  lifecycle {
-    prevent_destroy = true
-  }
+
 }
+
+# Zookeeper DNS records
 
 resource "aws_route53_record" "zookeeper_1" {
   zone_id = aws_route53_zone.domain.zone_id
@@ -29,4 +29,30 @@ resource "aws_route53_record" "zookeeper_3" {
   type    = "A"
   ttl     = 300
   records = [aws_instance.zookeeper.2.public_ip]
+}
+
+# Kafka DNS records
+
+resource "aws_route53_record" "kafka_1" {
+  zone_id = aws_route53_zone.domain.zone_id
+  name    = "kafka1.${var.domain_name}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.kafka.0.public_ip]
+}
+
+resource "aws_route53_record" "kafka_2" {
+  zone_id = aws_route53_zone.domain.zone_id
+  name    = "kafka2.${var.domain_name}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.kafka.1.public_ip]
+}
+
+resource "aws_route53_record" "kafka_3" {
+  zone_id = aws_route53_zone.domain.zone_id
+  name    = "kafka3.${var.domain_name}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.kafka.2.public_ip]
 }
